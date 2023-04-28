02b94ae057f6e2b264f26a217aea3f7ce2a01335
Giulia Occhini <okkio94@live.it>
2023-04-28 14:51:04 +0100

    slow progress

--- kinhom_work/parallel_simulations.py
+++ kinhom_work/parallel_simulations.py
@@ -69,37 +69,59 @@ df = pd.DataFrame({'laty': [laty], 'longy': [longy]})
 
 
 
-x_all = []
-y_all = []
-for i in range(2):
-    # Simulate a Poisson point process
-    x = []
-    y = []
-    n_accepted = 0
-    n_required = len(intang)
-    n_sim = 30000
-
-    while n_accepted < n_required:
-
-        xx = np.random.uniform(0, x_delta, n_sim) + x_min  # x coordinates of Poisson points
-        yy = np.random.uniform(0, y_delta, n_sim) + y_min  # y coordinates of Poisson points
-
-        # calculate spatially-dependent thinning probabilities
-        p = fun_p(kernel, xx, yy)
-
-        # Generate Bernoulli variables (ie coin flips) for thinning
-        booleThinned = np.random.uniform(0, 1, n_sim) < p  # points to be thinned
-        booleRetained = ~booleThinned  # points to be retained
-        x.append(xx[booleThinned])
-        y.append(yy[booleThinned])
-        n_accepted += sum(booleThinned)
-
-        # x/y locations of thinned points
-        xxThinned = xx[booleThinned]
-        yyThinned = yy[booleThinned]
-        # x/y locations of retained points
-        xxRetained = xx[booleRetained]
-        yyRetained = yy[booleRetained]
+def sampler(x, y, kernel, intang):
     
-    x_all.append(x)
-    y_all.append(x)
+    x_min = x.min()
+    x_max = x.max()
+    y_min = y.min()
+    y_max = y.max()
+    x_delta = x_max - x_min
+    y_delta = y_max - y_min 
+    area = x_delta*y_delta 
+
+    x_all = []
+    y_all = []
+    for i in range(0):
+        # Simulate a Poisson point process
+        lat = []
+        lon = []
+        n_accepted = 0
+        n_required = len(intang)
+        n_sim = len(intang) + 10**4
+
+        while n_accepted < n_required:
+
+            xx = np.random.uniform(0, x_delta, n_sim) + x_min  # x coordinates of Poisson points
+            yy = np.random.uniform(0, y_delta, n_sim) + y_min  # y coordinates of Poisson points
+
+            # calculate spatially-dependent thinning probabilities
+            p = fun_p(kernel, xx, yy)
+
+            # Generate Bernoulli variables (ie coin flips) for thinning
+            booleThinned = np.random.uniform(0, 1, n_sim) < p  # points to be thinned
+            booleRetained = ~booleThinned  # points to be retained
+            lat.append(xx[booleThinned])
+            lon.append(yy[booleThinned])
+            n_accepted += sum(booleThinned)
+
+            # x/y locations of thinned points
+            xxThinned = xx[booleThinned]
+            yyThinned = yy[booleThinned]
+            # x/y locations of retained points
+            xxRetained = xx[booleRetained]
+            yyRetained = yy[booleRetained]
+        
+        x_all.append(lat)
+        y_all.append(lon)
+    return x_all, y_all
+
+def parallelize_df(df, function):
+    n_cores = os.cpu_count()
+    df_split = np.array_split(df, n_cores)
+    with get_context('fork').Pool(processes = n_cores) as pool:
+        df = pd.concat(pool.map(function, df_split))
+        pool.close()
+        pool.join()
+    return df
+
+if __name__ == '__main__':
